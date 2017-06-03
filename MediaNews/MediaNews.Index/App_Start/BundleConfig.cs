305f5ab581d6c7093d4e93f78b867a0c340155e5
Miroslav Kirilov Miloradov <sappy996@gmail.com>
2017-06-03 20:27:06 +0300

    Change BundleConfig

--- MediaNews/MediaNews.Index/App_Start/BundleConfig.cs
+++ MediaNews/MediaNews.Index/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace MediaNews.Index
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/css/style.css"));
+                      "~/Content/style.css"));
         }
     }
 }
